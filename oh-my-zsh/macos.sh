4c534fc62d6e001c30e14b9d0d0df43f7276d9b1
d0u9 <d0u9.su@outlook.com>
2016-05-03 18:41:47 +0800

    oh_my_zsh: Add osx plugin supports.

--- oh-my-zsh/macos.sh
+++ oh-my-zsh/macos.sh
@@ -1,3 +1,10 @@
 # This config file specific to OS X platform.
 # You can set any variables ONLY to OS X platform.
 
+## For plugins
+plugins+=(osx)
+
+if hash brew 2> /dev/null; then
+    plugins+=(brew)
+fi
+
