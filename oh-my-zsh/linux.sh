adb08bf4c5702dfb878c4381eab73de6499acc0e
Douglas Su <doug.su.au@gmail.com>
2023-02-28 06:10:28 +0000

    add linux homebrew support

--- oh-my-zsh/linux.sh
+++ oh-my-zsh/linux.sh
@@ -1,3 +1,8 @@
 # This config file specific to Linux platform.
 # You can set any variables ONLY to linux platform.
+plugins+=(brew)
 
+## Homebrew
+if [ -d /home/linuxbrew/.linuxbrew ]; then
+	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
