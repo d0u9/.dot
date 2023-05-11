ba4cb93b7b90a38c4bd3ef82c6bb1f00a713f219
Douglas Su <doug.su.au@gmail.com>
2023-05-11 13:29:01 +1000

    use global config dir

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,34 +1,58 @@
 -- init.lua
 -- User settings
+function script_path()
+  return debug.getinfo(2, "S").source:sub(2):match("(.*/)")
+end
 
-vim.opt.runtimepath = vim.opt.runtimepath + '~/.config/nvim/plugins/pack/packer/start/packer.nvim'
-vim.g.plugin_path = '~/.config/nvim/plugins'
-vim.opt.packpath = vim.opt.packpath + '~/.config/nvim/plugins'
+function init_lua_package_path(config_dir)
+  local dirs = {
+    "/init",
+    "/plugins",
+    "/key_mapping"
+  }
 
-vim.g.undo_dir = '~/.config/nvim/tmp_files/undo_cache'
-vim.g.backup_dir = '~/.config/nvim/tmp_files/backup_files'
-vim.g.swap_dir = '~/.config/nvim/tmp_files/swap_files'
-vim.g.dein_cache_dir = '~/.config/nvim/tmp_files/dein_cache'
-vim.g.neoyank_file = '~/.config/nvim/tmp_files/denite_files/yankring.txt'
-vim.g.neomru_dir = '~/.config/nvim/tmp_files/denite_files'
+  for _, dir in ipairs(dirs) do
+    vim.opt.packpath:append(config_dir .. dir)
+  end
+
+end
+
+-- Basic setings
+_G.CONFIG_DIR = script_path()
+
+
+vim.opt.runtimepath:append(_G.CONFIG_DIR .. '/plugins/pack/packer/start/packer.nvim')
+
+-- Used for searching packages (vim plugins)
+vim.opt.packpath:append(_G.CONFIG_DIR .. 'plugins')
+
+vim.g.undo_dir          = _G.CONFIG_DIR .. '~/.config/nvim/tmp_files/undo_cache'
+vim.g.backup_dir        = _G.CONFIG_DIR .. '~/.config/nvim/tmp_files/backup_files'
+vim.g.swap_dir          = _G.CONFIG_DIR .. '~/.config/nvim/tmp_files/swap_files'
+vim.g.dein_cache_dir    = _G.CONFIG_DIR .. '~/.config/nvim/tmp_files/dein_cache'
+vim.g.neoyank_file      = _G.CONFIG_DIR .. '~/.config/nvim/tmp_files/denite_files/yankring.txt'
+vim.g.neomru_dir        = _G.CONFIG_DIR .. '~/.config/nvim/tmp_files/denite_files'
+
+-- `package.path` is used to search lub modules.
+init_lua_package_path(_G.CONFIG_DIR)
 
 -- nvim plugins to be installed.
 require('plugins')
 
 -- nvim's basic settings
-vim.cmd('source $HOME/.config/nvim/basic_settings.vim')
+vim.cmd('source' .. _G.CONFIG_DIR .. '/basic_settings.vim')
 
 -- vimscript key mappings
-vim.cmd('source $HOME/.config/nvim/key_mappings.vim')
+vim.cmd('source' .. _G.CONFIG_DIR .. '/key_mappings.vim')
 
 -- nvim's file format
-vim.cmd('source $HOME/.config/nvim/file_formatting.vim')
+vim.cmd('source' .. _G.CONFIG_DIR .. '/file_formatting.vim')
 
 -- lua plugin settings
 require('plugin_settings')
 
 -- vimscript plugin settings
-vim.cmd('source $HOME/.config/nvim/plugin_settings.vim')
+vim.cmd('source' .. _G.CONFIG_DIR .. '/plugin_settings.vim')
 
 -- vimscript key mappings
-vim.cmd('source $HOME/.config/nvim/plugin_key_mappings.vim')
+vim.cmd('source' .. _G.CONFIG_DIR .. '/plugin_key_mappings.vim')
