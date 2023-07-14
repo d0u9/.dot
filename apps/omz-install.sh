5961d573522a1ec1bd19d1f70227546e815c8339
Douglas Su <doug.su.au@gmail.com>
2023-07-14 14:16:52 +1000

    Change d0u9.zsh-theme to d0u9-pre-20230717.zsh-theme

--- apps/omz-install.sh
+++ apps/omz-install.sh
@@ -10,19 +10,21 @@ info "Installing oh-my-zsh configurations"
 CONFIG_DIR=$(realpath "$HOME/.config")
 OMZ_DIR=$(realpath "$APP_DIR/omz")
 OMZ_CONF_FILE=$(realpath "$OMZ_DIR/zshrc")
-THEME_NAME="d0u9.zsh-theme"
-OMZ_THEME_FILE=$(realpath "$OMZ_DIR/oh-my-zsh_themes/$THEME_NAME")
+OMZ_THEME_DIR=$(realpath "$OMZ_DIR/themes")
 
 cd $HOME
 back_or_override .zshrc
 
-rm -f .zshrc
-ln -s "$OMZ_CONF_FILE" .zshrc
+ln -fs "$OMZ_CONF_FILE" .zshrc
 
-info "Installing theme"
+info "Installing themes"
 cd $HOME/.oh-my-zsh/themes
-rm -f $THEME_NAME
-ln -s "$OMZ_THEME_FILE" .
+# Delete all dead links
+find . -type l ! -exec test -e {} \; -delete
+
+for file in "$OMZ_THEME_DIR"/*.zsh-theme; do
+    ln -fs "$file" .
+done
 
 info "Installing zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
